cfa1f2e4ab95f7c3ee9c931690d36a9b70d2629c
Kenneth Gibb Wraight <kenneth.gibb.wraight@cern.ch>
2025-03-24 14:50:16 +0000

    example using hack input

--- userPages/themeC/page1.py
+++ userPages/themeC/page1.py
@@ -15,3 +15,25 @@ class Page1(Page):
         pageDict=super().main()
 
         st.write("## Hello C1")
+
+        # test hack file
+        if "df_hack" in pageDict.keys():
+            st.info("Using __hack__ input")
+            if st.checkbox("Show hack data"):
+                st.write(pageDict['df_hack'])
+            for i,row in pageDict['df_hack'].iterrows():
+                pageDict[row['key']+'_hack']=row['value']
+
+        selOpts=["A","B","C"]
+        valKey="thing"
+        if valKey+'_hack' not in pageDict.keys():
+            pageDict[valKey]=st.radio(f"Select a {valKey}:",options=selOpts)
+        else:
+            if pageDict[valKey+'_hack'] not in selOpts:
+                st.write(f"Unexpected cached value ({pageDict[valKey+'_hack']}). Ignoring...")
+                pageDict[valKey]=st.radio(f"Select a {valKey}:",options=selOpts)
+            else:
+                pageDict[valKey]=st.radio(f"Select a {valKey}:",options=selOpts,index=selOpts.index(pageDict[row['key']+'_hack']))
+
+        st.write("Selected value:", pageDict[valKey])
+    
