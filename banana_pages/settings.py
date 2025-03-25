2de858891da8a39a8149e9850bb98d2640f778d2
Kenneth Gibb Wraight <kenneth.gibb.wraight@cern.ch>
2025-03-25 00:40:41 +0000

    test content

---
+++ banana_pages/settings.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.header("Settings")
+st.write(f"__Current__ theme: {st.session_state.theme}.")
---
+++ banana_pages/themeB/page1.py
@@ -0,0 +1,13 @@
+### standard
+import streamlit as st
+### custom
+
+#####################
+### main part
+#####################
+
+# st.set_page_config(page_title="Simple Page", page_icon="📊")
+
+st.title("Zeroth Page")
+st.sidebar.header("pageB1")
+st.write("## Hello B1")
