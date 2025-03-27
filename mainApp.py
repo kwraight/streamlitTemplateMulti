8ae406c962b8d3d9140ae76c12f13fa0dcbac6d6
Kenneth Gibb Wraight <kenneth.gibb.wraight@cern.ch>
2025-03-27 14:15:27 +0000

    Create mainApp.py

---
+++ mainApp.py
@@ -0,0 +1,12 @@
+from core.MultiApp import App
+
+smalls={
+    'git':"https://github.com/kwraight/streamlitTemplateMulti",
+    'current commit': ' - built with: COMMITCODE',
+    #'docker':"https://hub.docker.com/repository/docker/kwraight/multitemplate-app",
+    'other':"otherstuff"
+}
+
+myapp = App("multiApp", "Streamlit Multi Theme App", smalls)
+
+myapp.main()
