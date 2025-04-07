4a66955735d0cd52587db5fedbc084612c70cee1
Kenneth Gibb Wraight <kenneth.gibb.wraight@cern.ch>
2025-04-07 22:20:45 +0100

    consistent placeholder

--- mainApp.py
+++ mainApp.py
@@ -2,7 +2,7 @@ from core.MultiApp import App
 
 smalls={
     'git':"https://github.com/kwraight/streamlitTemplateMulti",
-    'current commit': ' - built with: COMMITCODE',
+    'current commit': ' - built with: COMMIT_CODE',
     #'docker':"https://hub.docker.com/repository/docker/kwraight/multitemplate-app",
     'other':"otherstuff"
 }
